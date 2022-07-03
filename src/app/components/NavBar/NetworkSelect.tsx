1f1332016dc630cacf068058b8e3a2456095f6ed
Ayagoumi <agoumi94@gmail.com>
2022-07-03 17:34:13 +0100

    Update: Added selected Network component

--- src/app/components/NavBar/NetworkSelect.tsx
+++ src/app/components/NavBar/NetworkSelect.tsx
@@ -3,57 +3,64 @@ import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 import { Box } from '@mui/material';
-import { useTheme } from '@mui/material';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import { getCchainStatus } from 'store/cchainSlice';
 import { useSelector } from 'react-redux';
 
+function SelectedNetwork({
+  value,
+  networkStatus,
+}: {
+  value: string;
+  networkStatus: string;
+}) {
+  return (
+    <Box sx={{ display: 'flex', gap: 1 }}>
+      <FiberManualRecordIcon
+        color={networkStatus === 'failed' ? 'error' : 'success'}
+        style={{ width: '12px' }}
+      />
+      <Box
+        style={{
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+      >
+        {value}
+      </Box>
+    </Box>
+  );
+}
+
 export default function NetworkSelect() {
-  const theme = useTheme();
   const [network, setNetwork] = React.useState('Columbus');
+  const status = useSelector(getCchainStatus);
+
   const handleChange = (event: SelectChangeEvent) => {
     setNetwork(event.target.value as string);
   };
-  const status = useSelector(getCchainStatus);
-  console.log('status', status);
 
   return (
-    <Box>
+    <Box
+      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    >
       <Select
         variant="outlined"
         value={network}
         onChange={handleChange}
         IconComponent={KeyboardArrowDownRoundedIcon}
         renderValue={value => {
-          return (
-            <Box sx={{ display: 'flex', gap: 1 }}>
-              <FiberManualRecordIcon
-                color={status === 'failed' ? 'error' : 'success'}
-                style={{ width: '12px' }}
-              />
-              <Box
-                style={{
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                }}
-              >
-                {value}
-              </Box>
-            </Box>
-          );
+          return <SelectedNetwork value={value} networkStatus={status} />;
         }}
         sx={{
           height: '40px',
           maxWidth: '250px',
-          minWidth: '150px',
+          minWidth: '170px',
           borderRadius: '10px',
           padding: '8px 16px',
-          [theme.breakpoints.down('md')]: {
-            minWidth: '125px',
-          },
-          [theme.breakpoints.down('xs')]: {
-            maxWidth: '50px !important',
-            minWidth: '50px !important',
+          '@media (max-width:370px)': {
+            minWidth: '120px',
+            width: '120px',
           },
         }}
       >
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,4 +4,18 @@ export enum Timeframe {
   DAYS_7 = 'DAYS_7',
   MONTHS_1 = 'MONTHS_1',
 }
+
+export function getLabel(timeframe: Timeframe) {
+  switch (timeframe) {
+    case Timeframe.HOURS_24:
+      return '24 Hours';
+    case Timeframe.DAYS_7:
+      return '7 Days';
+    case Timeframe.MONTHS_1:
+      return '1 Month';
+    default:
+      return 'UNKNOWN';
+  }
+}
+
 export type { RootState };
