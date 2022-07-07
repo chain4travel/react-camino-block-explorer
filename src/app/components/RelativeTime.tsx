8b0ad2de0d5bef98bc98c811ad9a1b4b202b177c
Ayagoumi <agoumi94@gmail.com>
2022-07-07 09:37:20 +0100

    Update: add relative time component

---
+++ src/app/components/RelativeTime.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Typography, Tooltip } from '@mui/material';
+import { getRelativeTime } from 'utils/display/display-utils';
+
+export default function RelativeTime({
+  variant,
+  value,
+}: {
+  variant?: 'cam' | 'gas';
+  value: number;
+}) {
+  const date = new Date(value);
+  return (
+    <Tooltip title={date.toLocaleString('en-US')}>
+      <Typography variant="subtitle2" color="latestList.timestamp">
+        {getRelativeTime(value) + ' ago'}
+      </Typography>
+    </Tooltip>
+  );
+}
