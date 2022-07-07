d05fffb1670ba48e43345733ffb63dcea6a290e7
Ayagoumi <agoumi94@gmail.com>
2022-07-07 09:50:28 +0100

    Refactor: removal of the footer, till later commits.

--- src/app/Layout/MainLayout.tsx/index.tsx
+++ src/app/Layout/MainLayout.tsx/index.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import { NavBar } from 'app/components/NavBar';
 import { Outlet } from 'react-router-dom';
-import { Typography } from '@mui/material';
 
 export default function MainLayout() {
   return (
     <>
       <NavBar />
       <Outlet />
-      <Typography variant="h6" gutterBottom>
-        Footer
-      </Typography>
     </>
   );
 }
