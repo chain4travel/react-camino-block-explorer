a3f8a8300f9dec110a8cd4542aede9c6a5a171aa
Ayagoumi <agoumi94@gmail.com>
2022-07-07 18:42:47 +0100

    Feat(component): created a component to hold reload button and radio buttons

---
+++ src/app/components/DataControllers.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import RowRadioButtonsGroup from './RowRadioButtonsGroup';
+import GlobalReloadButton from './GlobalReloadButton';
+
+export default function DataControllers() {
+  return (
+    <Grid container spacing={{ xs: 1, md: 1 }}>
+      <Grid item xs>
+        <RowRadioButtonsGroup />
+      </Grid>
+      <Grid item xs="auto" sm={3}>
+        <GlobalReloadButton style={{ display: 'flex', marginLeft: 'auto' }} />
+      </Grid>
+    </Grid>
+  );
+}
