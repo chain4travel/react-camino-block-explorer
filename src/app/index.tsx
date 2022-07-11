9259b4a902348bca7318daa82cfa12e074f52c1c
Ayagoumi <agoumi94@gmail.com>
2022-07-11 23:16:16 +0100

    Fix: Added back the global style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -56,6 +56,7 @@ export function App() {
         </Route>
         {/* <Route path="/notfound" element={<NotFoundPage />} /> */}
       </Routes>
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
