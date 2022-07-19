674cfab752ab242668cbbc2a30d57b503ec07e59
Ayagoumi <agoumi94@gmail.com>
2022-07-19 17:39:07 +0100

    Feat: add footer

--- src/app/Layout/MainLayout.tsx/index.tsx
+++ src/app/Layout/MainLayout.tsx/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { NavBar } from 'app/components/NavBar';
 import { Outlet } from 'react-router-dom';
+import { Footer } from 'app/components/Footer';
 
 export default function MainLayout() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
