5b7e5afc19d2719ec4ced0582deedff30888eac1
Ayagoumi <agoumi94@gmail.com>
2022-07-28 03:33:25 +0100

    Update components

--- src/app/components/CopyTitleCard.tsx
+++ src/app/components/CopyTitleCard.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Typography, Grid } from '@mui/material';
-import CopyToClipboardButton from 'app/components/CopyToClipboardButton';
 import Icon from '@mdi/react';
 import OutlinedContainer from 'app/components/OutlinedContainer';
+import DetailsField from 'app/components/DetailsField';
 
 export default function CopyTitleCard({
   label,
@@ -15,59 +14,19 @@ export default function CopyTitleCard({
 }) {
   return (
     <OutlinedContainer transparent={false}>
-      <Grid
-        container
-        columnSpacing={2}
-        sx={{ display: 'flex', alignItems: 'flex-end', padding: '1rem' }}
-        wrap="nowrap"
-      >
-        {label && (
-          <Grid
-            container
-            item
-            xs="auto"
-            direction="row"
-            sx={{ display: 'flex', alignItems: 'center' }}
-          >
-            <Icon path={icon} size={1} color="latestList.iconColor" />
-            <Typography
-              variant="h6"
-              component="h6"
-              color="textPrimary"
-              fontWeight="fontWeightBold"
-              sx={{ marginLeft: '10px' }}
-            >
-              {label}
-            </Typography>
-          </Grid>
-        )}
-        <Grid
-          item
-          xs
-          zeroMinWidth
-          sx={{
-            display: 'flex',
-            alignItems: 'flex-end',
-          }}
-        >
-          <Typography
-            variant="body1"
-            component="span"
-            fontWeight="fontWeightBold"
-            color="latestList.timestamp"
-            noWrap={true}
-            sx={{
-              display: 'inline-block',
-              width: '100%',
-            }}
-          >
-            {value}
-          </Typography>
-        </Grid>
-        <Grid item xs="auto">
-          <CopyToClipboardButton value={value} />
-        </Grid>
-      </Grid>
+      <DetailsField
+        field={label}
+        value={value}
+        type="string"
+        icon={
+          <Icon
+            path={icon}
+            color="latestList.iconColor"
+            style={{ width: '20px', height: '20px' }}
+          />
+        }
+        allowCopy={true}
+      />
     </OutlinedContainer>
   );
 }
