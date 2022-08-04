4bc7bc01e11a890c449d903b942825fe36d0fe6d
Ayagoumi <agoumi94@gmail.com>
2022-08-05 00:53:56 +0100

    Update failed network change error message.

--- src/app/Layout/MainLayout/index.tsx
+++ src/app/Layout/MainLayout/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { NavBar } from 'app/components/NavBar';
 import { Outlet } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'store/configureStore';
@@ -6,17 +6,23 @@ import {
   getActiveNetwork,
   selectAllChains,
   selectNetworkStatus,
+  changeNetwork,
 } from 'store/app-config';
 import { Footer } from 'app/components/Footer';
 import { Status } from 'types';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { getChains } from 'api';
+import PageContainer from 'app/components/PageContainer';
+import MainButton from 'app/components/MainButton';
 
 export default function MainLayout() {
   const chains = useAppSelector(selectAllChains);
   const status = useAppSelector(selectNetworkStatus);
   const activeNetwork = useAppSelector(getActiveNetwork);
   const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(changeNetwork('Columbus'));
+  };
   React.useEffect(() => {
     dispatch(getChains());
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -26,9 +32,30 @@ export default function MainLayout() {
       <NavBar />
       {status === Status.SUCCEEDED && chains?.length > 0 && <Outlet />}
       {status === Status.FAILED && (
-        <Typography variant="h6" component="span">
-          Something went wrong, Please try later!
-        </Typography>
+        <PageContainer pageTitle="Error" metaContent="An error has occurred">
+          <Box
+            sx={{
+              display: 'flex',
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              gap: '20px',
+            }}
+          >
+            <Typography
+              variant="h4"
+              component="span"
+              fontWeight="fontWeightBold"
+              sx={{ color: 'error.light' }}
+            >
+              Something went wrong, Please Again!
+            </Typography>
+            <MainButton variant="contained" onClick={handleClick}>
+              Switch to Columbus Network
+            </MainButton>
+          </Box>
+        </PageContainer>
       )}
       <Footer />
     </>
