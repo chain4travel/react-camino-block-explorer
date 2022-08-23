93ea13e43b786c71781fd9cf9e75f2afe4608491
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-08-23 23:23:46 +0100

    change layout structer

--- src/app/Layout/MainLayout/index.tsx
+++ src/app/Layout/MainLayout/index.tsx
@@ -15,56 +15,63 @@ import { getChains } from 'api';
 import PageContainer from 'app/components/PageContainer';
 import MainButton from 'app/components/MainButton';
 
-export default function MainLayout() {
+const Content = () => {
   const chains = useAppSelector(selectAllChains);
   const status = useAppSelector(selectNetworkStatus);
-  const activeNetwork = useAppSelector(getActiveNetwork);
   const dispatch = useAppDispatch();
   const handleClick = () => {
     dispatch(changeNetwork('Columbus'));
   };
+  if (status === Status.LOADING || status === Status.IDLE)
+    return (
+      <Container fixed maxWidth="xl">
+        <CircularProgress
+          color="secondary"
+          size={75}
+          style={{ margin: 'auto', display: 'block' }}
+        />
+      </Container>
+    );
+  else if (status === Status.SUCCEEDED && chains?.length > 0) return <Outlet />;
+  return (
+    <PageContainer pageTitle="Error" metaContent="An error has occurred">
+      <Box
+        sx={{
+          display: 'flex',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          gap: '20px',
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="span"
+          fontWeight="fontWeightBold"
+          sx={{ color: 'error.light' }}
+        >
+          Something went wrong, Please Again!
+        </Typography>
+        <MainButton variant="contained" onClick={handleClick}>
+          Switch to Columbus Network
+        </MainButton>
+      </Box>
+    </PageContainer>
+  );
+};
+
+export default function MainLayout() {
+  const activeNetwork = useAppSelector(getActiveNetwork);
+  const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(getChains());
   }, [activeNetwork]); // eslint-disable-line
+
   return (
     <>
       <NavBar />
-      {status === Status.LOADING && (
-        <Container fixed maxWidth="xl">
-          <CircularProgress
-            color="secondary"
-            size={75}
-            style={{ margin: 'auto', display: 'block' }}
-          />
-        </Container>
-      )}
-      {status === Status.SUCCEEDED && chains?.length > 0 && <Outlet />}
-      {status === Status.FAILED && (
-        <PageContainer pageTitle="Error" metaContent="An error has occurred">
-          <Box
-            sx={{
-              display: 'flex',
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-              gap: '20px',
-            }}
-          >
-            <Typography
-              variant="h4"
-              component="span"
-              fontWeight="fontWeightBold"
-              sx={{ color: 'error.light' }}
-            >
-              Something went wrong, Please Again!
-            </Typography>
-            <MainButton variant="contained" onClick={handleClick}>
-              Switch to Columbus Network
-            </MainButton>
-          </Box>
-        </PageContainer>
-      )}
+      <Content />
       <Footer />
     </>
   );
