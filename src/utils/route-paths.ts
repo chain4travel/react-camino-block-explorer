c04bb5c8d1e06948165f2ded5f9d414bea56d560
Ayagoumi <agoumi94@gmail.com>
2022-11-25 11:34:44 +0100

    fix: broken social media links

--- src/utils/route-paths.ts
+++ src/utils/route-paths.ts
@@ -30,11 +30,11 @@ export const WALLET = 'https://wallet.camino.foundation/';
 
 export const DOCS = 'https://docs.camino.foundation/';
 
-export const DISCORD = 'https://discord.gg/K5THjAweFB';
+export const DISCORD = 'https://discord.gg/camino';
 
-export const TWITTER = 'https://twitter.com/CaminoFndtn';
+export const TWITTER = 'https://twitter.com/caminonetwork';
 
-export const TELEGRAM = 'https://t.me/caminochain';
+export const TELEGRAM = 'https://t.me/camino_network';
 
 export const MEDIUM = 'https://medium.com/@caminofoundation';
 
