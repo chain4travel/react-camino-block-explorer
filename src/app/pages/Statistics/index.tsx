af503c2976e85408e974aa08c5d6ae39f25bfbf0
Esteban Londoño Ospina <stebanlondo75@gmail.com>
2023-02-23 15:46:30 -0500

    delete first test json

--- src/app/pages/Statistics/index.tsx
+++ src/app/pages/Statistics/index.tsx
@@ -78,9 +78,9 @@ const Statistics: FC = () => {
         />
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={2}>
-            <Grid item md={6} xs={12}>
+            {/* <Grid item md={6} xs={12}>
               <BlockchainCharts
-                tooltipTitle="The chart shows the number of Camino tokens transferred daily."
+                tooltipTitle="The Camino Average Gas Limit Chart shows the historical daily average gas limit of the Camino network."
                 darkMode={dark}
                 titleText={'Gas Limit'}
                 utilSlice={(dates) => loadGasAverageLimit(dates)}
@@ -88,7 +88,7 @@ const Statistics: FC = () => {
                 sliceGetterLoader={getGasAverageLimitInfo}
                 typeStatistic={typeBlockchainDataChart.GAS_AVERAGE_LIMIT}
               />
-            </Grid>
+            </Grid> */}
             <Grid item md={6} xs={12}>
               <BlockchainCharts
                 tooltipTitle="This chart highlights the total number of transactions on the Camino blockchain with daily individual breakdown for estimated hash rate, average block time and size, total block and uncle block count and total new address seen."
