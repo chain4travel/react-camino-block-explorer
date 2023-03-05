588ca26febd1b7ec46477c844bcf2f4ef42aad46
Ayagoumi <agoumi94@gmail.com>
2023-03-05 15:58:58 +0100

    style: update zIndex of the AppBar

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,15 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
-let timestamp = Date.now()
 const publicPath = process.env.PUBLIC_PATH
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map',
-    cache: false,
 
     output: {
         publicPath: publicPath,
-        filename: 'js/[name].[fullhash:8].' + timestamp + '.js',
-        chunkFilename: 'js/[name].[fullhash:8].' + timestamp + '.js',
+        filename: 'js/[name].[fullhash:8].js',
+        chunkFilename: 'js/[name].[fullhash:8].js',
     },
 
     performance: {
