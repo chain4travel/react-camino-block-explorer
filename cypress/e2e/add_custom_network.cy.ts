1fbb4367eef1f9ecf742ae602f6fe28751aa903b
riversoft-henry.lu <henry.lu@gmail.com>
2023-03-09 15:21:07 +0800

    test(explorer): e2e add custom network

---
+++ cypress/e2e/add_custom_network.cy.ts
@@ -0,0 +1,7 @@
+describe('Explorer: Add Custom Network', { tags: ['@explorer'] }, () => {
+    context('normal cases: ', { tags: ['@columbus', '@kopernikus'] }, () => {
+        it('verify to add custom network workflow', () => {
+            cy.entryExplorer()
+        })
+    })
+})
