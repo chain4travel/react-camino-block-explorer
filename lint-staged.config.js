c792de5df7693ad486ef3bbdb26475b51fabeae4
Ayagoumi <agoumi94@gmail.com>
2023-03-16 10:36:40 +0100

    fix(lint): adding the src files lint check

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ module.exports = {
     '*.{js,jsx,ts,tsx}': ['eslint --ext .ts,.tsx ./src', 'eslint'],
     // '**/*.ts?(x)': () => 'npm run check-types',
     '*.{json,yaml}': ['prettier --write'],
+    '*.{js,jsx,ts,tsx}': ['prettier --write ./src/**/*.{js,jsx,ts,tsx}'],
 }
