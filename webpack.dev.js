81893a436ce244d3a3787c853ff77d2b05a31966
Mohamed Elmoslemany <117270519+mo-c4t@users.noreply.github.com>
2023-06-06 17:57:18 +0300

    use code debugger for dev environment (#151)
    * use code debugger for dev environment

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,21 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
+const publicPath = process.env.PUBLIC_PATH
+
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
 
-    devServer: {
-        port: 5002,
-        historyApiFallback: true,
-        static: './dist',
+    output: {
+        publicPath: publicPath,
+        filename: 'js/[name].[fullhash:8].js',
+        chunkFilename: 'js/[name].[fullhash:8].js',
+    },
+
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
     },
 })
